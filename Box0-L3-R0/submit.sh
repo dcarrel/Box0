766d1c130be0620c25734e96eb2860ddb5114499
dcarrel1 <58124391+dcarrel1@users.noreply.github.com>
2021-07-13 16:44:05 -0700

    Update submit.sh

--- Box0-L3-R0/submit.sh
+++ Box0-L3-R0/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J Box0-L3-R1
-#SBATCH -o Box0-L3-R1.out
-#SBATCH -e Box0-L3-R1.err
+#SBATCH -J Box0-L3-R0
+#SBATCH -o Box0-L3-R0.out
+#SBATCH -e Box0-L3-R0.err
 #SBATCH -N 4
-#SBATCH -n 64
-#SBATCH -p normal
-#SBATCH -t 12:00:00
+#SBATCH -n 128
+#SBATCH -p development
+#SBATCH -t 2:00:00
 #SBATCH --mail-user=carrel@ucsb.edu
 #SBATCH --mail-type=all
 module load intel
@@ -13,4 +13,4 @@ module load hdf5
 module load mvapich2
 
 cd $SLURM_SUBMIT_DIR
-ibrun ../enzo.exe -d -r DD0002/data0002
+ibrun ../enzo.exe -d Box0-L3-R0.enzo
